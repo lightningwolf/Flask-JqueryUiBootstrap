a4e7a430f5e0c5b1eae48584f7427ccbb5d9ecbd
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2013-08-24 01:08:47 +0200

    integration with jquery-ui-bootstrap

--- flask_jqueryuibootstrap/__init__.py
+++ flask_jqueryuibootstrap/__init__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding=utf8
 
-__version__ = '0.0.1'
-
 from flask import Blueprint, current_app, url_for
 
 try:
@@ -15,51 +13,14 @@ else:
         return isinstance(field, HiddenField)
 
 
-def bootstrap_find_resource(filename,
-                            use_minified=None,
-                            cdn='bootstrap'):
-    # FIXME: get rid of this function and instead manipulate the flask routing
-    #        system
-    config = current_app.config
-
-    if None == use_minified:
-        use_minified = config['BOOTSTRAP_USE_MINIFIED']
-
-    if use_minified:
-        filename = '%s.min.%s' % tuple(filename.rsplit('.', 1))
-
-    if not config['BOOTSTRAP_USE_CDN']:
-        return url_for('bootstrap.static', filename=filename)
-    else:
-        baseurl = config['BOOTSTRAP_CDN_BASEURL'][cdn]
-
-        if baseurl.startswith('//') and config['BOOTSTRAP_CDN_PREFER_SSL']:
-            baseurl = 'https:%s' % baseurl
-        return baseurl + filename
-
-
 class Bootstrap(object):
     def __init__(self, app=None):
         if app is not None:
             self.init_app(app)
 
     def init_app(self, app):
-        app.config.setdefault('BOOTSTRAP_USE_MINIFIED', True)
-        app.config.setdefault('BOOTSTRAP_JQUERY_VERSION', '1')
         app.config.setdefault('BOOTSTRAP_HTML5_SHIM', True)
         app.config.setdefault('BOOTSTRAP_GOOGLE_ANALYTICS_ACCOUNT', None)
-        app.config.setdefault('BOOTSTRAP_USE_CDN', False)
-        app.config.setdefault('BOOTSTRAP_CDN_PREFER_SSL', True)
-        app.config.setdefault('BOOTSTRAP_FONTAWESOME', False)
-        app.config.setdefault('BOOTSTRAP_CUSTOM_CSS', False)
-        app.config.setdefault(
-            'BOOTSTRAP_CDN_BASEURL', {
-                'bootstrap':   '//cdnjs.cloudflare.com/ajax/libs'
-                               '/twitter-bootstrap/2.3.2/',
-                'fontawesome': '//cdnjs.cloudflare.com/ajax/libs'
-                               '/font-awesome/3.0.2/',
-            }
-        )
 
         blueprint = Blueprint(
             'bootstrap',
@@ -72,5 +33,3 @@ class Bootstrap(object):
 
         app.jinja_env.filters['bootstrap_is_hidden_field'] =\
             is_hidden_field_filter
-        app.jinja_env.filters['bootstrap_find_resource'] =\
-            bootstrap_find_resource
