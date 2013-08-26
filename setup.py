2565b780b25c4d575d68c9b24a7fa6ed632c54e0
Arkadiusz Tułodziecki <atulodzi@gmail.com>
2013-08-26 23:13:45 +0200

    change homepage to Github

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def main():
         description=__description__,
         long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
         license="MIT",
-        url="http://lightningwolf.net/flask-jqueryuibootstrap",
+        url="https://github.com/lightningwolf/Flask-JqueryUiBootstrap",
         packages=['flask_jqueryuibootstrap'],
         zip_safe=False,
         include_package_data=True,
